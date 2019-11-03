d7d3286cf36b8207f6737b34aa198ab9957204f4
pch18 <pch18@qq.com>
2019-11-03 17:29:07 +0900

    some things

--- mapgather_with_cartographer/main_config.bash
+++ mapgather_with_cartographer/main_config.bash
@@ -1,5 +1,7 @@
+DIR_MAP_GLOBAL="${HOME}/ws_pch18/kuaro_map"
 DIR_MAP_LOCAL="/1101/first"
-DIR_MAP_GLOBAL="~/ros_ws/kuaro_mapdata"
 
 export DIR_MAP="${DIR_MAP_GLOBAL}${DIR_MAP_LOCAL}"
 export DIR_GATHER=`dirname $BASH_SOURCE`
+
+mkdir -p $DIR_MAP
