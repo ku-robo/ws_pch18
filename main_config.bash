4b500bf7010b74703680eb5f804495423670829e
pch18 <pch18@qq.com>
2019-11-03 18:06:37 +0900

    改名

--- main_config.bash
+++ main_config.bash
@@ -2,6 +2,5 @@ DIR_MAP_GLOBAL="${HOME}/ws_pch18/kuaro_map"
 DIR_MAP_LOCAL="/1101/first"
 
 export DIR_MAP="${DIR_MAP_GLOBAL}${DIR_MAP_LOCAL}"
-export DIR_GATHER=`dirname $BASH_SOURCE`
 
 mkdir -p $DIR_MAP
